787ba3b67fa4d7d3667efd2f7b73d6b5e9c45060
Duke.wu <497160282@qq.com>
2019-08-14 14:57:17 +0800

    [   FEATURE  ] watch 添加

---
+++ FunnyFMForWatch Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  FunnyFMForWatch Extension
+//
+//  Created by Duke on 2019/8/14.
+//  Copyright © 2019 Duke. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
