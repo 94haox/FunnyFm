3ecd09dda532ada7bda3efdbd95d31a60978878e
Tao <nslogwutao@hotmail.com>
2020-03-18 10:39:26 +0800

    [FEATURE] 添加章节解析初步

---
+++ FunnyFm/Models/Chapter.swift
@@ -0,0 +1,17 @@
+//
+//  Chapter.swift
+//  FunnyFm
+//
+//  Created by wt on 2020/3/17.
+//  Copyright © 2020 Duke. All rights reserved.
+//
+
+import UIKit
+
+class Chapter: NSObject {
+    
+    init(time: CMTime, title: String, image: UIImage) {
+        
+    }
+
+}
