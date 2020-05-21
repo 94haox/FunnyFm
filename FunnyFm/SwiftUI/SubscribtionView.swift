372a9767bfc4aa8bbfcf6c417b3cd6cddefc2f6b
吴涛 <497160282@qq.com>
2020-05-21 19:10:32 +0800

    feat |> 添加原生 term 和 policy

---
+++ FunnyFm/SwiftUI/SubscribtionView.swift
@@ -0,0 +1,21 @@
+//
+//  SubscribtionView.swift
+//  FunnyFm
+//
+//  Created by 吴涛 on 2020/5/21.
+//  Copyright © 2020 Duke. All rights reserved.
+//
+
+import SwiftUI
+
+struct SubscribtionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SubscribtionView_Previews: PreviewProvider {
+    static var previews: some View {
+        SubscribtionView()
+    }
+}
---
+++ FunnyFm/SwiftUI/TermView.swift
@@ -0,0 +1,21 @@
+//
+//  TermView.swift
+//  FunnyFm
+//
+//  Created by 吴涛 on 2020/5/20.
+//  Copyright © 2020 Duke. All rights reserved.
+//
+
+import SwiftUI
+
+struct TermView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TermView_Previews: PreviewProvider {
+    static var previews: some View {
+        TermView()
+    }
+}
