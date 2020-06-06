9c432f0e45ea604c3a039fd1db36e5fec8e17e10
吴涛 <497160282@qq.com>
2020-06-06 23:23:52 +0800

    feat |> 添加 OPML 方式

---
+++ FunnyFm/Controllers/OpmlListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OpmlListViewController.swift
+//  FunnyFm
+//
+//  Created by 吴涛 on 2020/6/6.
+//  Copyright © 2020 Duke. All rights reserved.
+//
+
+import UIKit
+
+class OpmlListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
